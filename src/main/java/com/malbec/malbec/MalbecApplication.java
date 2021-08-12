7cb241c690d2677868e032267ca569fb798a6f2a
soollan <soollan@tmon.co.kr>
2021-08-12 21:00:46 +0900

    init

---
+++ src/main/java/com/malbec/malbec/MalbecApplication.java
@@ -0,0 +1,13 @@
+package com.malbec.malbec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalbecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalbecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malbec/malbec/MalbecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malbec.malbec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalbecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
